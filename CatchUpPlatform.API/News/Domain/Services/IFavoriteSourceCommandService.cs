f46d23791c5ca718849c523982770fcde2274b6d
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-21 17:29:21 -0500

    refactor(namespaces): fix namespace paths for favorite source services.

--- CatchUpPlatform.API/News/Domain/Services/IFavoriteSourceCommandService.cs
+++ CatchUpPlatform.API/News/Domain/Services/IFavoriteSourceCommandService.cs
@@ -1,7 +1,7 @@
 using CatchUpPlatform.API.News.Domain.Model.Aggregates;
 using CatchUpPlatform.API.News.Domain.Model.Commands;
 
-namespace CatchUpPlatform.API.News.Domain.Model.Services;
+namespace CatchUpPlatform.API.News.Domain.Services;
 
 public interface IFavoriteSourceCommandService
 {
--- CatchUpPlatform.API/News/Domain/Services/IFavoriteSourceQueryService.cs
+++ CatchUpPlatform.API/News/Domain/Services/IFavoriteSourceQueryService.cs
@@ -1,7 +1,7 @@
 using CatchUpPlatform.API.News.Domain.Model.Aggregates;
 using CatchUpPlatform.API.News.Domain.Model.Queries;
 
-namespace CatchUpPlatform.API.News.Domain.Model.Services;
+namespace CatchUpPlatform.API.News.Domain.Services;
 
 public interface IFavoriteSourceQueryService
 {
