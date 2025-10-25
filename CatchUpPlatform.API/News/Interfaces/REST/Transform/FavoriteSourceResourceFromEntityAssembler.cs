1a89d8c7c3f23678fd8037c1f5d227f7db4ed828
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-25 09:17:47 -0500

    feat(favorite-source): add assembler for converting favorite source entities to resources.

---
+++ CatchUpPlatform.API/News/Interfaces/REST/Transform/FavoriteSourceResourceFromEntityAssembler.cs
@@ -0,0 +1,10 @@
+using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+using CatchUpPlatform.API.News.Interfaces.REST.Resources;
+
+namespace CatchUpPlatform.API.News.Interfaces.REST.Transform;
+
+public static class FavoriteSourceResourceFromEntityAssembler
+{
+    public static FavoriteSourceResource ToResourceFromEntity(FavoriteSource entity) =>
+        new(entity.Id, entity.NewsApiKey, entity.SourceId); 
+}
